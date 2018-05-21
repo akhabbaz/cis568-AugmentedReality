bd84d4a5b60e698c23129d65ad0faae703bfc0cb
Anton Khabbaz <anton.khabbaz@gmail.com>
2018-05-21 10:18:21 -0400

    Transfer Vector3

---
+++ Assets/Scripts/TransferVector3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate byte[] SerializeMethod(object customObject)
+{
+
+}
+public class TransferVector3 : Photon.MonoBehaviour {
+
+    public static readonly byte[] memVec3 = new byte[3 * 4]; 
+    private static short SerializeVector3(StreamBuffer streamBuffer, ) 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
